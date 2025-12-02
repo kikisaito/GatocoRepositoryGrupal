ede0da0a74224f8096adb87f037a4f20b11bd6b5
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-12-02 10:44:30 -0600

    Revert API URL to include port 8080

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     // Asegúrate de que termine con :8080 si ese es tu puerto
-    apiUrl: 'http://44.195.177.19',
+    apiUrl: 'http://44.195.177.19:8080',
     apiVersion: 'v1',
     appName: 'GatoCo',
     appVersion: '1.0.0'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://44.195.177.19',
+    apiUrl: 'http://44.195.177.19:8080',
     apiVersion: 'v1',
     appName: 'GatoCo',
     appVersion: '0.0.0'
