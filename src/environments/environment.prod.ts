86e0566f28cb5540c4f6750eb431f00196833126
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-12-02 11:46:13 -0600

    fix: update apiUrl to new IP and add /api/v1 path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     // Asegúrate de que termine con :8080 si ese es tu puerto
-    apiUrl: 'http://44.195.177.19:8080',
+    apiUrl: 'http://54.83.45.10:8080/api/v1',
     apiVersion: 'v1',
     appName: 'GatoCo',
     appVersion: '1.0.0'
