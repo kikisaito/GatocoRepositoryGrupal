aabf1f3d6d5da0f227e22bcd6a92b94089adb970
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-12-01 19:07:34 -0600

    Update API URL to AWS IP

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://44.221.66.236:8080/api/v1', // Apunta a tu backend local
+    apiVersion: 'v1',
+    appName: 'GatoCo',
+    appVersion: '1.0.0'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api/v1',
+    apiUrl: 'http://44.221.66.236:8080/api/v1',
     apiVersion: 'v1',
     appName: 'GatoCo',
     appVersion: '0.0.0'
