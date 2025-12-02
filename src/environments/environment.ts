e565f8c9c3a6e61cbf7f1a67c13475e8b8432e24
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-12-02 12:20:16 -0600

    fix: correct apiUrl to use Server IP instead of DB IP

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://54.83.45.10:8080/api/v1',
+    apiUrl: 'http://44.195.177.19:8080/api/v1',
     apiVersion: 'v1',
     appName: 'GatoCo',
     appVersion: '0.0.0'
